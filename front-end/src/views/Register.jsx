56612d1f0e24fa7b6ce4ee9c53c573260dd14c2f
ivan-nedelchev <130435484+ivan-nedelchev@users.noreply.github.com>
2024-04-15 16:18:58 +0300

    Added password verification and error msg on Register

--- front-end/src/views/Register.jsx
+++ front-end/src/views/Register.jsx
@@ -6,6 +6,7 @@ import { saveUser } from '../controllers/auth';
 
 const Register = ({ setAuthenticated }) => {
     const [formData, setFormData] = useState({})
+    const [passwordMatching, setPasswordMatching] = useState(true);
     const navigate = useNavigate();
     const saveInput = (ev) => {
         const inputName = ev.target.name;
@@ -18,18 +19,25 @@ const Register = ({ setAuthenticated }) => {
 
     const registerUser = async (ev) => {
         ev.preventDefault();
-        let userFile = await post('/register', { ...formData })
-        if (userFile) {
-            console.log(userFile);
-            saveUser(userFile);
-            setAuthenticated(true);
-            navigate('/');
+        if (formData.password != formData.repass) {
+            setPasswordMatching(false)
+        } else {
+            let userFile = await post('/register', { ...formData })
+            if (userFile) {
+                console.log(userFile);
+                saveUser(userFile);
+                setAuthenticated(true);
+                navigate('/');
+            }
         }
     }
     return (
         <>
             <div className='registerForm'>
-                <h3>Register Page</h3>
+                <h3 style={{ padding: '20px' }}>Register Page</h3>
+                {!passwordMatching &&
+                    <div className='error-msg'>Passwords don't match!</div>
+                }
                 <form onSubmit={registerUser} action="/register" method="post">
                     <label htmlFor="username">Username:</label>
                     <input
@@ -47,13 +55,13 @@ const Register = ({ setAuthenticated }) => {
                         name="password"
                         pattern="^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])\S{8,}$"
                         title="Password must contain at least one number, 
-                   one alphabet, one symbol, and be at 
-                   least 8 characters long"
+                        one alphabet, one symbol, and be at 
+                        least 8 characters long"
                         onChange={saveInput}
                         required
                     />
                     <label htmlFor="repassword"
-                    >Re-type Password:</label>
+                    >Repeat Password:</label>
                     <input
                         type="password"
                         id="repassword"
