f56d73651f9ade7c4992c3691c82daf57117218c
Ivan Nedelchev <130435484+ivan-nedelchev@users.noreply.github.com>
2024-09-21 23:57:53 +0300

    Added backgroud image

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import CarList from "./components/CarList";
 import { fetchCars } from "../utils/cars";
 import CarSearch from "./components/CarSearch";
-
+import "./Home.css";
 const Home = () => {
   const [cars, setCars] = useState([]);
   useEffect(() => {
@@ -13,11 +13,10 @@ const Home = () => {
     getCars();
   }, []);
   return (
-    <>
+    <div className="home-container">
       <CarSearch setCars={setCars}></CarSearch>
       <CarList cars={cars} />
-    </>
+    </div>
   );
 };
-
 export default Home;
