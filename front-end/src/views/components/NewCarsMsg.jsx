b1cc72a02346ba4efe547ff17d19aa67f5321f8d
Ivan Nedelchev <130435484+ivan-nedelchev@users.noreply.github.com>
2024-09-26 18:27:31 +0300

    Add bouncing image

--- front-end/src/views/components/NewCarsMsg.jsx
+++ front-end/src/views/components/NewCarsMsg.jsx
@@ -3,7 +3,12 @@ import "./NewCarsMsg.css";
 const NewCarsMsg = () => {
   return (
     <div className="container cars-msg">
-      <h3>Newest car listings</h3>
+      <h3 className="newCar-title">Newest car listings:</h3>
+      <img
+        src="http://localhost:5555/images/arrow_down_png.png"
+        alt="Bounce arrow"
+        className="bounce-arrow"
+      ></img>
     </div>
   );
 };
